18dbafc3ed489bd76e8cf130b2b82fa4836e3eea
Risky [ Zero Tow ] <santuyaja019@gmail.com>
2022-02-08 16:42:24 +0700

    Update jmbf.py

--- jmbf.py
+++ jmbf.py
@@ -4,6 +4,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("jmbf-enc.py").Main()
+       __import__("jmbf-enc").Main()
    except Exception as e:
        exit(str(e))
